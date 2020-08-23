df872677ebe7f3e9dec3ac721078359abff95688
amr639 <amr.639@gmail.com>
2020-08-23 20:07:45 +0530

    handling url path requests

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
 const laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-type": "text/html" });
-  res.end("This is the response!");
+  const pathName = url.parse(req.url, true).pathname;
+
+  if (pathName === "/products" || pathName === "" || pathName === "/") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end("This is the PRODUCTS page");
+  } else if (pathName === "/laptop") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end("This is the LAPTOP page");
+  } else {
+    res.writeHead(404, { "Content-type": "text/html" });
+    res.end("Url was not found on the server");
+  }
 });
 
 server.listen(1337, "127.0.0.1", () => {
